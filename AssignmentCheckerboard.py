533abfa82c344d1ec98c96724e28331a45bf7a66
Tommy <tommyhongthai@gmail.com>
2017-05-09 16:23:39 -0700

    my first python repo

---
+++ AssignmentCheckerboard.py
@@ -0,0 +1,54 @@
+# for i in range(0,9):
+#     if i % 2 == 0:
+#         print '* * * * '
+#     else:
+#         print ' * * * *'
+
+# def multiply(arr,num):
+#     print arr, num
+#     for x in arr:
+#         x *= num
+#     # return arr
+# a = [2,4,10,16]
+# b = multiply(a,5)
+# print b
+
+# def multiply(arr,num):
+#     print arr, num
+#     for x in arr:
+#         print x
+#         x *= num
+#     return arr
+# a = [2,4,10,16]
+# b = multiply(a,5)
+# print b
+
+# def multiply(arr,num):
+#     print arr, num # output should be [2,4,10,16] 5
+#     for x in arr:
+#         print x
+#         x *= num
+#         print x
+#     return arr
+# a = [2,4,10,16]
+# b = multiply(a,5)
+# print b
+
+# sq = 10 
+# for num in rdange (100,1001):
+#     b = "Bar"
+#     # pergect quare
+#     if num == sq * sq:
+#         print num, 'Bar'
+#         sq+=1
+#     # fine prim
+#     is_prime=Truei 
+#     i=2
+#     while is_prime and i < num:
+#             ifnum % 1 = 0:
+#                 is_prime = False
+#             i += 1
+#     if is_prime:
+#         b = ""
+#         print num, "Foo"
+#     print num, f+b
